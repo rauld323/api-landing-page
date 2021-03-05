5d3cbd252d2b05d8e0f0164c86c21034b0133755
Raul <rauldavila323@gmail.com>
2021-03-05 17:15:27 +0100

    Intro img for mobile has been finished. Navbar needs to worked on.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+	var x = document.getElementById("siteLinks");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
