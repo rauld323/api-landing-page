f181228d7b3ce9723c66c57c47531881bd6eee5e
Raul <rauldavila323@gmail.com>
2021-04-07 15:31:38 +0200

    Navbar has been reworked so it uses bootstrap. Info boxes will also boot strap.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-function myFunction() {
-	var x = document.getElementById("siteLinks");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "block";
-	}
-}
-
-
-
 	// const newBox = document.querySelector('new-box');
 
 
